1f88f4b4799fb54bb818bbed2e393f2456fda016
Saison <157907297+SAI-SON@users.noreply.github.com>
2025-10-25 21:14:59 +0530

    Time: 3 ms (23.92%) | Memory: 45.7 MB (32.95%) - LeetSync

---
+++ 2398-check-if-matrix-is-x-matrix/check-if-matrix-is-x-matrix.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean checkXMatrix(int[][] grid) {
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[i].length;j++){
+                if(i==j || i==grid[i].length-1-j){
+                    if(grid[i][j]==0) return false;
+                }
+                else if(grid[i][j]!=0) return false;
+            }
+        }
+        return true;
+    }
+}
