dce64897dfd4d8c0cc2053e5368adc849ecac6af
Saison <157907297+SAI-SON@users.noreply.github.com>
2025-12-07 09:58:15 +0530

    Time: 188 ms (59.64%) | Memory: 46.5 MB (62.76%) - LeetSync

---
+++ 4085-largest-prime-from-consecutive-prime-sum/largest-prime-from-consecutive-prime-sum.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int largestPrime(int n) {
+        boolean isPrime[]=new boolean[n+1];
+        Arrays.fill(isPrime,true);
+        isPrime[0]=false;
+        isPrime[1]=false;
+        for(int i=2;i*i<=n;i++){
+            if(isPrime[i]){
+                for(int j=i*i;j<=n;j+=i){
+                    isPrime[j]=false;
+                }
+            }
+        }
+     
+        int sum=0,l=0;
+        for(int i=2;i<=n;i++){
+            if(isPrime[i]){
+                sum+=i;
+                if(sum>n)
+                break;
+                if(isPrime[sum])
+                l=sum;
+            }
+        }
+        return l;
+    }
+}
