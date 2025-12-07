10dd46801a9f5f1fe6ec319024948b0138d7e5f1
Saison <157907297+SAI-SON@users.noreply.github.com>
2025-12-07 10:11:12 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (29.32%) - LeetSync

--- 1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.java
+++ 1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.java
@@ -1,10 +1,11 @@
 class Solution {
     public int countOdds(int low, int high) {
-        int c=0;
-        for(int i=low;i<=high;i++){
-            if((i&1)==1)
-            c++;
-        }
-        return c;
+        // int c=0;
+        // for(int i=low;i<=high;i++){
+        //     if((i&1)==1)
+        //     c++;
+        // }
+        // return c;
+        return (high+1)/2 - (low/2);
     }
 }
