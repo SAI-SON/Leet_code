38d49088d2435b07aac43b2ed04d5bd2ac81a0d2
Saison <157907297+SAI-SON@users.noreply.github.com>
2025-12-27 19:47:52 +0530

    Time: 41 ms (96.32%) | Memory: 109.2 MB (69.95%) - LeetSync

---
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long maximumHappinessSum(int[] happiness, int k) {
+        Arrays.sort(happiness);
+         long sum=0;
+         long val=0;
+         for(int i=0;i<k;i++){
+            long last=happiness[happiness.length-i-1];
+            last-=val;
+            if(last<=0) break;
+            sum+=last;
+            val++;
+         }
+         return sum;
+    }
+}
