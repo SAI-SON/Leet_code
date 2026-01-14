968e24522b0bfd8d818107d719b8d4d102162844
Saison <157907297+SAI-SON@users.noreply.github.com>
2026-01-14 19:41:02 +0530

    Time: 40 ms (86.79%) | Memory: 56.7 MB (21.28%) - LeetSync

---
+++ 2761-array-reduce-transformation/array-reduce-transformation.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    // let res=init;
+    for(let n of nums){
+        init=fn(init,n);
+    }
+    return init;
+    // return nums.reduce(fn,init);
+};
