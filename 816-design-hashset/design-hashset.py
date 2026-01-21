7245c88e089f996ce1bbabb99f8cfdeac95abf8a
Saison <157907297+SAI-SON@users.noreply.github.com>
2026-01-21 21:02:24 +0530

    Time: 82 ms (49.21%) | Memory: 40.2 MB (6.35%) - LeetSync

---
+++ 816-design-hashset/design-hashset.py
@@ -0,0 +1,33 @@
+class MyHashSet(object):
+
+    def __init__(self):
+        self.mp=[False]*1000001
+
+    def add(self, key):
+        """
+        :type key: int
+        :rtype: None
+        """
+        self.mp[key]=True
+        
+
+    def remove(self, key):
+        """
+        :type key: int
+        :rtype: None
+        """
+        self.mp[key]=False
+
+    def contains(self, key):
+        """
+        :type key: int
+        :rtype: bool
+        """
+        return self.mp[key]
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
