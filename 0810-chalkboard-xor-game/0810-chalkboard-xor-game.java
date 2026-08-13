0b7f99e2de27b4a79dd780bba6e5a3a5713637da
Saison <157907297+SAI-SON@users.noreply.github.com>
2026-08-13 12:02:46 +0530

    Time: 0 ms (100%), Space: 46.1 MB (89.14%) - LeetHub

---
+++ 0810-chalkboard-xor-game/0810-chalkboard-xor-game.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean xorGame(int[] nums) {
+        int xor=0;
+        for(int i:nums){
+            xor^=i;
+        }
+        if(xor==0) return true;
+        return nums.length%2==0;
+    }
+}
