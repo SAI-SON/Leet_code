0a5d3cf818db8ba831ec1af3cf935faf164a1979
Saison <157907297+SAI-SON@users.noreply.github.com>
2026-08-13 12:15:58 +0530

    Time: 162 ms (7.14%), Space: 117.5 MB (11.04%) - LeetHub

---
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minGroups(int[][] intervals) {
+        Arrays.sort(intervals,(a,b)->{
+            return a[0]-b[0];
+            // return b[1]-a[1];
+        });
+        System.out.println(Arrays.deepToString(intervals));
+       PriorityQueue<Integer>pq=new PriorityQueue<>();
+       for(int []interval:intervals){
+        int start=interval[0];
+        int end=interval[1];
+        if(!pq.isEmpty() && pq.peek()<start){
+            pq.poll();
+        }
+        pq.add(end);
+       }
+        return pq.size();
+    }
+}
